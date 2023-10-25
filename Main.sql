144d057344b7d8eef61781990b604f26e18348e2
Vladifish <101858972+Vladifish@users.noreply.github.com>
2023-10-25 18:29:16 +0800

    ADDED: Question 1 Query

--- Main.sql
+++ Main.sql
@@ -1,1 +1,8 @@
 --Enter SQL Codes here
+
+-- Question 1 --
+-- Find the titles of the courses in the Computer Science department that have 3 Credits.
+
+SELECT TITLE
+FROM COURSE 
+WHERE DEPT_NAME LIKE "Comp. Sci." AND CREDITS = 3; 
