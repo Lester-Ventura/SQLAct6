28f9596ff877ed608c3f471589efe614a005688d
LVen <132431646+Lester-Ventura@users.noreply.github.com>
2023-10-25 22:31:28 +0800

    Update and rename Question2a to Question2a.sql

---
+++ Question2a.sql
@@ -0,0 +1,9 @@
+--2a. Find the total grade points earned by the student with ID '66054 across all
+courses taken by the student.
+
+SELECT SUM(credits * points) AS total_grade_points
+FROM takes
+NATURAL JOIN course
+NATURAL JOIN grade_points
+WHERE ID = '66054';
+
