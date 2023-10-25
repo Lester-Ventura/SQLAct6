6969ad36be2c817e0c95d2538f8fd6281cb94217
Vladifish <101858972+Vladifish@users.noreply.github.com>
2023-10-25 19:08:34 +0800

    ADDED: question b, einstein query, EDITED: question a. of part 1

--- Main.sql
+++ Main.sql
@@ -1,8 +1,19 @@
 --Enter SQL Codes here
 
--- Question 1 --
+-- Part 1:
+
+-- a. --
 -- Find the titles of the courses in the Computer Science department that have 3 Credits.
 
 SELECT TITLE
 FROM COURSE 
 WHERE DEPT_NAME LIKE "Comp. Sci." AND CREDITS = 3; 
+
+
+-- b.
+-- Find the IDs of all students who were taught by instructor Einstein; make sure no
+-- duplicates are shown in the result(s).
+
+SELECT s_ID as ID
+FROM ADVISOR 
+WHERE i_ID = 22222; -- this is einstein's instructor ID
