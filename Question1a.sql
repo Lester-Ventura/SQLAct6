6b3bbdb56758f1a85280a2717153fd109584f0d8
LVen <132431646+Lester-Ventura@users.noreply.github.com>
2023-10-25 22:11:58 +0800

    Update and rename Question1a to Question1a.sql

--- Question1a.sql
+++ Question1a.sql
@@ -1,2 +1,5 @@
--- 1a. --
+-- 1a.
 -- Find the titles of the courses in the Computer Science department that have 3 Credits.
+SELECT TITLE 
+FROM COURSE 
+WHERE DEPT_NAME = “Comp Sci.” AND CREDITS = 3; 
