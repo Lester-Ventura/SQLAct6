cbd54c0f99ce38e78363ff6b0a3b98f002ff4779
LVen <132431646+Lester-Ventura@users.noreply.github.com>
2023-10-25 22:26:01 +0800

    Create Question5a.sql

---
+++ Question5a.sql
@@ -0,0 +1,10 @@
+/*a. Display the grade for each student based on the mark relation. Tip: use
+case - when structures.
+select
+case
+when <condition> then <value>
+...
+
+else <value> //may not be necessary
+end
+…*/
