be9fd90053845953d2ae174ca89ad1143e73c278
LVen <132431646+Lester-Ventura@users.noreply.github.com>
2023-10-27 16:31:22 +0800

    Update and rename Question3c to Question3c.sql

--- Question3c.sql
+++ Question3c.sql
@@ -1,2 +1,6 @@
 -- 3c. Insert every student whose 𝑡𝑜𝑡_𝑐𝑟𝑒𝑑 attribute is greater than 100 as an
 -- instructor in the same department with a salary of $10,000.
+INSERT INTO instructor (id, name, dept_name, salary) 
+SELECT id, name, dept_name , 10000 
+FROM student 
+WHERE tot_cred > 100;
