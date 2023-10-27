8691d674c163d527056172525d3bf98c781d8378
LVen <132431646+Lester-Ventura@users.noreply.github.com>
2023-10-27 15:42:25 +0800

    Update and rename Question1b to Question1b.sql

--- Question1b.sql
+++ Question1b.sql
@@ -1,5 +1,6 @@
 -- 1b.
 -- Find the IDs of all students who were taught by instructor Einstein; make sure no
 -- duplicates are shown in the result(s).
-
+SELECT DISTINCT ID FROM takes 
+WHERE course_id = (SELECT course_id FROM teaches WHERE ID = '10101');
 
