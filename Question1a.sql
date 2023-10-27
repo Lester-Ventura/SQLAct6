6f5ecd743d84376c3677c49d2bfbac27195b7462
LVen <132431646+Lester-Ventura@users.noreply.github.com>
2023-10-27 15:34:58 +0800

    Updated Dept_Name = "Comp Sci." -> "Comp. Sci.". Changed " appropriately.

--- Question1a.sql
+++ Question1a.sql
@@ -2,4 +2,4 @@
 -- Find the titles of the courses in the Computer Science department that have 3 Credits.
 SELECT TITLE 
 FROM COURSE 
-WHERE DEPT_NAME = “Comp Sci.” AND CREDITS = 3; 
+WHERE DEPT_NAME = "Comp. Sci." AND CREDITS = 3; 
