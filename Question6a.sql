b6760acc0f50c3f548ae9432fe19ca37ee8181ca
Tabuzo, Enzo <74560055+etdvprg@users.noreply.github.com>
2023-10-28 14:29:33 +0800

    UPDATE: Question6a.sql, v.1.0 (alpha)
    industrially coded by @etdvprg @casscodes22o

---
+++ Question6a.sql
@@ -0,0 +1,9 @@
+SELECT i.ID as FacultyID,
+i.name as FacultyName,
+i.dept_name as FacultyDept,
+COUNT(DISTINCT s.ID) AS NumberOfStudents
+FROM instructor i 
+LEFT JOIN teaches AS t ON i.ID = t.ID
+LEFT JOIN takes AS tk ON t.course_id = tk.course_id
+LEFT JOIN student AS s ON tk.ID = s.ID 
+GROUP BY FacultyID, FacultyName, FacultyDept;
