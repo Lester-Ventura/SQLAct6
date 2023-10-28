8dda98501bad2b75d0fe19e0a429301b2ed299f4
Vladifish <101858972+Vladifish@users.noreply.github.com>
2023-10-28 11:54:01 +0800

    ADDED: grade points table for question 2

--- Question2a.sql
+++ Question2a.sql
@@ -1,6 +1,26 @@
 --2a. Find the total grade points earned by the student with ID '66054 across all
 -- courses taken by the student.
 
+-- could just be ran in a different file, but doing it here for clarity
+-- also because the files are isolated in this case
+CREATE TABLE grade_points (
+		grade CHAR(2) PRIMARY KEY,
+    		points DECIMAL (3,1)
+    		);
+
+DELETE from grade_points;
+    
+INSERT INTO grade_points (grade, points) VALUES
+    		('A', 4.0),
+    		('A-', 3.7),
+    		('B+', 3.3),
+    		('B', 3.0),
+    		('B-', 2.7),
+    		('C+', 2.3),
+    		('C', 2.0),
+    		('D', 1.0),
+    		('F', 0.0);
+
 SELECT SUM(credits * points) AS total_grade_points
 FROM takes
 NATURAL JOIN course
--- Question2b.sql
+++ Question2b.sql
@@ -1,5 +1,27 @@
 --2b. Find the grade point average for the student above (GPA is the total grade point
 --divided by the total credits for the associated courses).
+
+
+-- could just be ran in a different file, but doing it here for clarity
+-- also because the files are isolated in this case
+CREATE TABLE grade_points (
+		grade CHAR(2) PRIMARY KEY,
+    		points DECIMAL (3,1)
+    		);
+
+DELETE from grade_points;
+    
+INSERT INTO grade_points (grade, points) VALUES
+    		('A', 4.0),
+    		('A-', 3.7),
+    		('B+', 3.3),
+    		('B', 3.0),
+    		('B-', 2.7),
+    		('C+', 2.3),
+    		('C', 2.0),
+    		('D', 1.0),
+    		('F', 0.0);
+        
 SELECT SUM(credits * points)/SUM(credits) AS GPA
 FROM takes
 NATURAL JOIN course
